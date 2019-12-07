31f7bd7035aacdf21c8a470b829ca6145724e632
Kazune Takahashi <kazunetakahashi@gmail.com>
2019-12-07 18:32:47 +0900

    vector Chap30 cont.

--- vector/main.cpp
+++ vector/main.cpp
@@ -6,9 +6,9 @@ class vector
 public:
   using value_type = T;
   using pointer = T *;
-  using const_pointer = const T *;
-  using refernce = value_type &;
-  using const_reference = const value_type &;
+  using const_pointer = T const *;
+  using reference = value_type &;
+  using const_reference = value_type const &;
   using allocator_type = Allocator;
   using size_type = std::size_t;
   using difference_type = std::ptrdiff_t;
@@ -24,6 +24,7 @@ private:
   allocator_type alloc;
 
 public:
+  // iterators
   iterator begin() noexcept { return first; }
   iterator end() noexcept { return last; }
   iterator begin() const noexcept { return first; }
@@ -34,8 +35,45 @@ public:
   reverse_iterator rend() noexcept { return reverse_iterator{first}; }
   reverse_const_iterator crbegin() const noexcept { return reverse_iterator{last}; }
   reverse_const_iterator crend() const noexcept { return reverse_iterator{first}; }
+  // size
+  size_type size() const noexcept { return std::distance(begin(), end()); }
+  bool empty() const noexcept { return begin() == end(); }
+  size_type capacity() const noexcept { return reserved_last - first; }
+  // access to the elements
+  reference operator[](size_type i) { return first[i]; }
+  const_reference operator[](size_type i) const { return first[i]; }
+  reference at(size_type i);
+  const_reference at(size_type i) const;
+  reference front() { return *first; }
+  const_reference front() const { return *first; }
+  reference back() { return *(last - 1); }
+  const_reference back() const { return *(last - 1); }
+  pointer data() { return first; }
+  const_pointer data() const { return first; }
 };
+
+template <typename T, typename Allocator>
+typename vector<T, Allocator>::reference vector<T, Allocator>::at(size_type i)
+{
+  if (i >= size())
+  {
+    throw std::out_of_range("index is out of range.");
+  }
+  return this[i];
+}
+
+template <typename T, typename Allocator>
+typename vector<T, Allocator>::const_reference vector<T, Allocator>::at(size_type i) const
+{
+  if (i >= size())
+  {
+    throw std::out_of_range("index is out of range.");
+  }
+  return this[i];
+}
 
 int main()
 {
+  vector<int> V;
+  int x = V[0];
 }
